0daa9f933605bc8461c49af6bd0b310bca613c7f
Marlene Sedano <marlenesedanodasilva@gmail.com>
2020-11-26 16:56:16 -0400

    Contador de Caracteres com JQuery

---
+++ Contador_de_Caracteres/js/app.js
@@ -0,0 +1,10 @@
+$('textarea').on('keyup paste', function () {
+    setTimeout(function () {
+        var valorTextarea = $('textarea').val().length,
+            palavras = $('textarea').val().split(' ').length;
+        $('.caracteres').text(valorTextarea);
+        $('.palavras').text(palavras);
+        console.log(palavras);
+    })
+
+});
